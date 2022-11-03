82e94a1cbda1efad3816c2eb504f07edc529494a
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2022-11-03 18:22:54 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/bnb/',
   plugins: [vue(), vueJsx()],
   css: {
     preprocessorOptions: {
